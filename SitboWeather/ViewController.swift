94a241ae252ed7a218223bb244e7d735a97444b8
zauTT <giozau1@gmail.com>
2025-07-01 13:55:04 +0400

    Initial Commit

---
+++ SitboWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SitboWeather
+//
+//  Created by Giorgi Zautashvili on 01.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SitboWeatherUITests/SitboWeatherUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SitboWeatherUITestsLaunchTests.swift
+//  SitboWeatherUITests
+//
+//  Created by Giorgi Zautashvili on 01.07.25.
+//
+
+import XCTest
+
+final class SitboWeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
